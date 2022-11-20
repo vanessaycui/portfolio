46bf6598dd240936b9668ddd03660af75afb3717
Vanessa <vanessa.y.cui@gmail.com>
2022-11-20 01:01:01 -0500

    adding css and formatting and js for small screens

--- js/main.js
+++ js/main.js
@@ -2,35 +2,24 @@
 /*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
-const bioBtn = document.querySelector("#bio-btn")
-const workBtn = document.querySelector("#work-btn")
-const schoolBtn = document.querySelector("#school-btn")
+
+const jobTabs = document.querySelector('.job-tabs')
+const allTabs = document.querySelectorAll('.tab')
 const resumeBtn = document.querySelector("#resume-btn")
-const bioSection = document.querySelector("#about-bio")
-const workSection = document.querySelector("#about-work")
-const schoolSection = document.querySelector('#about-school')
 
 /*----- event listeners -----*/
 
-bioBtn.addEventListener("click", (event)=>{
-    event.preventDefault()
-    bioSection.style.display = "flex"
-    workSection.style.display = "none"
-    schoolSection.style.display = "none"
-})
 
-workBtn.addEventListener("click", (event)=>{
-    event.preventDefault()
-    bioSection.style.display = "none"
-    workSection.style.display = "flex"
-    schoolSection.style.display = "none"
-})
-
-schoolBtn.addEventListener("click", (event)=>{
-    event.preventDefault()
-    bioSection.style.display = "none"
-    workSection.style.display = "none"
-    schoolSection.style.display = "flex"
+jobTabs.addEventListener("click", (event)=>{
+    console.dir(event.target)
+    allTabs.forEach(tab => {
+        tab.classList.remove("active")
+        let section = tab.id.split("-")[1]
+        document.querySelector('#'+section).classList.add("work-edu-hide")
+    })
+    event.target.classList.add("active")
+    let section = event.target.id.split("-")[1]
+    document.querySelector('#'+section).classList.remove("work-edu-hide")  
 })
 
 
