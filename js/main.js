56ee96759ad97d54bca893f13098a7678b2bce97
Vanessa <vanessa.y.cui@gmail.com>
2022-12-09 23:39:59 -0500

    changing back to scroll page. not SPA.

--- js/main.js
+++ js/main.js
@@ -94,30 +94,30 @@ emailBtn.addEventListener("click", (event)=>{
     window.open('mailto:vanessa.y.cui@gmail.com')
 })
 
-navLinks.addEventListener('click', (event)=>{
+// navLinks.addEventListener('click', (event)=>{
     
-    allNavLinks.forEach(link=>{ 
-        if (link.id !==""){
-            document.querySelector('#'+link.id).classList.remove("nav-links-active")
-        }
-        document.querySelector(link.hash).style.display='none'
-    })
+//     allNavLinks.forEach(link=>{ 
+//         if (link.id !==""){
+//             document.querySelector('#'+link.id).classList.remove("nav-links-active")
+//         }
+//         document.querySelector(link.hash).style.display='none'
+//     })
 
-    if (event.target.tagName ==='A'){
+//     if (event.target.tagName ==='A'){
         
-        document.querySelector(event.target.hash).style.display='block'
-        document.querySelector(event.target.hash).classList.add("fade-in")       
-    }
-    document.querySelector('#'+event.target.id).classList.add("nav-links-active")
-})
+//         document.querySelector(event.target.hash).style.display='block'
+//         document.querySelector(event.target.hash).classList.add("fade-in")       
+//     }
+//     document.querySelector('#'+event.target.id).classList.add("nav-links-active")
+// })
 
-navTitle.addEventListener('click', (event)=>{
-    allNavLinks.forEach(link=>{
-        document.querySelector(link.hash).style.display='none'
-    })
-    document.querySelector("#intro").style.display='flex'
+// navTitle.addEventListener('click', (event)=>{
+//     allNavLinks.forEach(link=>{
+//         document.querySelector(link.hash).style.display='none'
+//     })
+//     document.querySelector("#intro").style.display='flex'
 
-})
+// })
 
 
 
